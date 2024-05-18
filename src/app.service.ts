f13be38e14f33b6637cda25c127ed17ddcfa70ac
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-05-18 19:47:19 +0800

    feat: add logger for each response http

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger: Logger = new Logger(AppService.name);
   getHello(): string {
+    this.logger.verbose(`Get hello world response`);
     return 'Hello World!';
   }
 }
