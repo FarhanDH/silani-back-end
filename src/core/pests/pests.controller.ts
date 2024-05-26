fa8f6a54bff8c081ddd27c8c241f3990a82aa539
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2024-05-26 10:44:40 +0800

    feat(pests): add update service

--- src/core/pests/pests.controller.ts
+++ src/core/pests/pests.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreatePestDto } from './dto/create-pest.dto';
 import { PestsService } from './pests.service';
+import { UpdatePestDto } from './dto/update-pest.dto';
 
 @Controller('pests')
 export class PestsController {
@@ -8,11 +9,28 @@ export class PestsController {
 
   @Post()
   async create(@Body() createPestDto: CreatePestDto) {
-    return await this.pestsService.create(createPestDto);
+    const data = await this.pestsService.create(createPestDto);
+    return {
+      data,
+    };
   }
 
   @Get()
   async getAll() {
-    return await this.pestsService.getAll();
+    const data = await this.pestsService.getAll();
+    return {
+      data,
+    };
+  }
+
+  @Put(':pestId')
+  async update(
+    @Param('pestId') pestId: string,
+    @Body() updatePestDto: UpdatePestDto,
+  ) {
+    const data = await this.pestsService.update(pestId, updatePestDto);
+    return {
+      data,
+    };
   }
 }
--- src/core/pests/pests.service.ts
+++ src/core/pests/pests.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import { DrizzleService } from '~/common/drizzle/drizzle.service';
 import { pests } from '~/common/drizzle/schema';
 import { CreatePestDto } from './dto/create-pest.dto';
+import { UpdatePestDto } from './dto/update-pest.dto';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class PestsService {
@@ -19,5 +21,17 @@ export class PestsService {
   async getAll() {
     this.logger.debug(`PestsService.getAll()`);
     return await this.drizzleService.db.query.pests.findMany();
+  }
+
+  async update(pestId: string, updatePestDto: UpdatePestDto) {
+    this.logger.debug(
+      `PestsService.update(${pestId}, ${JSON.stringify(updatePestDto)})`,
+    );
+    return await this.drizzleService.db
+      .update(pests)
+      .set({ name: updatePestDto.name, updatedAt: new Date() })
+      .where(eq(pests.id, pestId))
+      .returning()
+      .then((data) => data[0]);
   }
 }
